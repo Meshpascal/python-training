45185abfffd3b2abae80939196fb446e95d2bd76
Meshpascal <meshacknyasiralivwege@gmail.com>
2026-04-23 00:10:15 +0300

    session 2 changes

---
+++ session1.py
@@ -0,0 +1,35 @@
+print ("hello world") # string datatype,enclosed in quotations
+
+student_name = "John" 
+
+age = 25
+
+marks = 90
+
+datatype_bool = True
+
+print(marks)
+
+no_1 = 10
+
+no_2 = 25
+
+no_3 = 40.5
+
+no_4 = 44.2
+
+print(no_1+no_3) # addition
+
+print(no_2-no_4) # subtraction
+
+print(no_2*no_3) # multiplication
+
+print(no_4/no_2) # division
+
+print(no_4//no_2) # eliminates the float(decimal)
+
+print(no_2%no_3)  # gives the remainder
+
+print(type(student_name)) # gives the datatype
+
+print(type(datatype_bool))
