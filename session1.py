2e3f765bf7b2ea0d38f2ff1aca5ba2ac28ff10f6
Meshpascal <meshacknyasiralivwege@gmail.com>
2026-04-29 18:24:47 +0300

    session 3

--- session1.py
+++ session1.py
@@ -1,4 +1,4 @@
-print ("hello world") # string datatype,enclosed in quotations
+"""""print ("hello world") # string datatype,enclosed in quotations
 
 student_name = "John" 
 
@@ -33,3 +33,47 @@ print(no_2%no_3)  # gives the remainder
 print(type(student_name)) # gives the datatype
 
 print(type(datatype_bool))
+
+print(100**1/2) # exponential"""
+
+# 2.1 VARIABLES
+
+#2.2 DATATYPES
+
+# 2.3 OPERATORS
+print(100>50) #greater than
+print(100<50)
+print(100>=50)
+print(100<=50)
+print(100!=50)
+
+#2.4 input and output
+# print and input are used for input and output
+
+id_number= input("enter id:  ")
+
+print("Your ID number is:"+ id_number)
+print(type(id_number))
+Id_2=int(input("enter id_2:  "))
+print(type(Id_2))
+print(float(50))
+
+#DATA STRUCTURES
+#list denoted by:[]separated by commas
+
+fruits=["banana,mango"]
+
+#tuples list that cannot be changed after creation denoted by ()
+
+fruit=("mango,banana,apples")
+
+#sets store unique items denoted by {} prevents duplication
+
+names={"warren,Lemein,Meingati"}
+
+#dictionary is just like sets but separated using a colon 
+student={
+    "reg:FGS35474,"
+    "AGE:25,"
+    "JOIN:2024"
+}
